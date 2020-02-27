5bd384c7225e0aec963dd69252a8e94b8891da2b
liuke-git <732446775@qq.com>
2020-02-28 00:52:56 +0800

    使用bootstrap做好导航栏

---
+++ src/main/java/com/liuke/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.liuke.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String hello(){
+        return "index";
+    }
+}
